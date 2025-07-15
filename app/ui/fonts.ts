c465c08312e3755ef0d2dc7b5ef38202fd17b305
ExoPolit <exopolit@gmx.de>
2025-07-15 19:41:29 +0200

    leck mich

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import {Inter, Lusitana, Roboto} from 'next/font/google'
+
+
+export const inter = Inter({ subsets: ['latin']});
+
+export const lusi = Lusitana({ weight: ['700']} );
+export const robot = Roboto({ weight: ['400']} );
